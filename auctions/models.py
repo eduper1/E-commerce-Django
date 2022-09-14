5f7b040efd6e26b92a79aba19bb141abd531cf1a
eduper1 <eduper.havard@gmail.com>
2022-09-14 15:08:37 +0300

    date_published ste to automatic

--- auctions/models.py
+++ auctions/models.py
@@ -38,7 +38,7 @@ class Comment(models.Model):
     comment_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commentor", blank=False)
     comment_on = models.ForeignKey( Auction_listings, related_name="comment", on_delete=models.CASCADE)
     comment = models.CharField(max_length=600)
-    comment_date_published = models.DateTimeField()
+    comment_date_published = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return f"{self.comment}"
