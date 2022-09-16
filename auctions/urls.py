8db394c56c75b3e98c5162110da43091fb9e889d
eduper1 <eduper.havard@gmail.com>
2022-09-16 15:58:25 +0300

    wacthlist route created

--- auctions/urls.py
+++ auctions/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
+    path("create", views.create_listing, name="create_listing"),
     path("<int:list_id>", views.list_page, name="auctions_list"),
     path("<int:list_id>/comment", views.comment, name="comment"),
-    path("create", views.create_listing, name="create_listing"),
+    path("watchlist", views.watch_list, name="watchlist" )
 ]
