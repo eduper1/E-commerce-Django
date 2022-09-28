25700322a03c3db94e88e6a164a733ccb7e1b9ad
eduper1 <eduper.havard@gmail.com>
2022-09-28 12:17:57 +0300

    update category admin

--- auctions/admin.py
+++ auctions/admin.py
@@ -9,7 +9,7 @@ class Comment_admin(admin.ModelAdmin):
     list_display = ("id", "comment_on", "comment")   
                                                                                                                                                                                                                                                                     
 class Bid_admin(admin.ModelAdmin):
-    list_display = ("id", "bid_by", "bid_on_auction", "bid_amount")   
+    list_display = ("id", "bid_by", "bid_on_auction", "place_bid", "bid_count")   
     
 
 class Category_admin(admin.ModelAdmin):
