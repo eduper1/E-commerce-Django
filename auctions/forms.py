9bb31a80f8552252f72b2c317859aea11374fc8d
eduper1 <eduper.havard@gmail.com>
2022-10-22 22:19:10 +0300

    Add labels, help_texts, widgets on Creating_listings forms

--- auctions/forms.py
+++ auctions/forms.py
@@ -6,6 +6,21 @@ class Create_listing(forms.ModelForm):
     class Meta:
         model = models.Auction_listings
         fields = ['auc_title', 'auc_details', 'auc_price', 'auctions', 'auc_image']
+        widgets = {
+            'auc_details': forms.Textarea(attrs={'cols': 50, 'rows': 5}),
+        }
+        labels = {
+            'auc_title': ('Item'),
+            'auc_details':('Description'),
+            'auc_price':('Price'),
+            'auctions':('Category'),
+            'auc_image':('Choose a Picture of the Item')
+        }
+        help_texts = {
+            'auc_title': ('Item name like: Watch, Book, etc.'),
+            'auc_details':("Describe the Item's feture"),
+            'auc_price':('Staring Bid'),
+        }
         
         
 class Create_comment(forms.ModelForm):
